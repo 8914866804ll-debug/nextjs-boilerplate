08acde5191914e30825a180fd7cf275fdc961631
8914866804ll-debug <8914866804ll@gmail.com>
2026-02-10 12:50:07 +0200

    1569

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 import HeroImage from "./components/hero/heroImage";
 import Main_Menu from "./components/hero/mainMenu";
 import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 1";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 2";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 3";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 4";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 5";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 6";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 7";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 8";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 9";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 10";
-import RecipeCarousel1 from "./components/carousels/RecipeCarousel copy 11";
+import RecipeCarousel2 from "./components/carousels/RecipeCarousel copy 2";
+import RecipeCarousel3 from "./components/carousels/RecipeCarousel copy 3";
+import RecipeCarousel4 from "./components/carousels/RecipeCarousel copy 4";
+import RecipeCarousel5 from "./components/carousels/RecipeCarousel copy 5";
+import RecipeCarousel6 from "./components/carousels/RecipeCarousel copy 6";
+import RecipeCarousel7 from "./components/carousels/RecipeCarousel copy 7";
+import RecipeCarousel8 from "./components/carousels/RecipeCarousel copy 8";
+import RecipeCarousel9 from "./components/carousels/RecipeCarousel copy 9";
+import RecipeCarousel10 from "./components/carousels/RecipeCarousel copy 10";
+import RecipeCarousel11 from "./components/carousels/RecipeCarousel copy 11";
 
 export type Recipe = {
   title: string;
