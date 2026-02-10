e8be47fc1a51e63272ec0eecd2047c474543ce47
8914866804ll-debug <8914866804ll@gmail.com>
2026-02-10 11:13:41 +0200

    pipec update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import Image from "next/image";
 import HeroImage from "./components/hero/heroImage";
 import Main_Menu from "./components/hero/mainMenu";
+import RecipeCarousel from "./components/hero/RecipeCarousel";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1a1a1a]"> 
       <HeroImage />
       <Main_Menu />
+
     </main>
   );
 }
