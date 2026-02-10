5651accef08a85fbae386390f3e2be459d72d637
8914866804ll-debug <8914866804ll@gmail.com>
2026-02-10 11:53:24 +0200

    2113

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import HeroImage from "./components/hero/heroImage";
 import Main_Menu from "./components/hero/mainMenu";
 import RecipeCarousel from "./components/hero/RecipeCarousel";
 
+export type Recipe = {
+  title: string;
+  desc: string;
+  price: string;
+  img: string;
+};
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1a1a1a]"> 
